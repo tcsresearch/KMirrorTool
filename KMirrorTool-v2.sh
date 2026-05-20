f3cffaca372a81e007f03f63a865e979308af8b4
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-20 00:05:32 -0500

    Refactor config and function sourcing in script

--- KMirrorTool-v2.sh
+++ KMirrorTool-v2.sh
@@ -2,19 +2,28 @@
 
 ### First, source our configs ###
 
-for $conf in config/*.conf do
-	source *.bfunc
-done
+function KMT_SourceConfigs() {
+	shopt -s nullglob	# prevents literal matches if no files exist.
+	for conf in config/*.conf do
+		source "$conf"
+	done
 
-echo " Configs Loaded"
+	echo " Configs Loaded"
+}
 
 ### Next, we source our functions ###
+function KMT_SourceFunctions() {
+	shopt -s nullglob	# prevents literal matches if no files exist.
+	for func in functions/*.bfunc do
+		source "$bfunc"
+	done
 
-for $func in functions/*.bfunc do
-	source *.bfunc
-done
+	echo "Functions Loaded."
+}
 
-echo "Functions Loaded."
+### Source Configs & Functions ###
+	KMT_SourceConfigs
+	KMT_SourceFunctions
 
 ### BLING Integration ###
 
@@ -24,7 +33,7 @@ echo "Functions Loaded."
 ### TODO: How do we call all these functions without creating a mess of code here?
 #	  Should each component have its own folder?  Then we call each folder with a for loop?
 #
-GetLatestKernelSources
-DuplicateKernels
+	GetLatestKernelSources
+	DuplicateKernels
 ExpiredKernels
 RCKernels
